9d6dfe4ecc7184718812435d03ac915a81cf6757
Marc Thomas <marc.thomas@thethomashouse.co.uk>
2014-12-26 14:38:56 +0000

    Got the basic application up and running with test data.

--- src/main/java/rota/RotaAppApplication.java
+++ src/main/java/rota/RotaAppApplication.java
@@ -2,8 +2,12 @@ package rota;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+
+import rota.dao.PersonRepository;
+import rota.entities.Person;
 
 @Configuration
 @ComponentScan
@@ -11,6 +15,11 @@ import org.springframework.context.annotation.Configuration;
 public class RotaAppApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(RotaAppApplication.class, args);
+        ConfigurableApplicationContext context = SpringApplication.run(RotaAppApplication.class, args);
+        PersonRepository repository = context.getBean(PersonRepository.class);
+
+        // save a couple of customers
+        repository.save(new Person("Mr", "Jack", "Bauer", "jack.bauer@ctu.gov"));
+        repository.save(new Person("Miss", "Chloe", "O'Brian", "chloe.obrian@ctu.gov"));
     }
 }
--- src/main/java/rota/dao/PersonRepository.java
+++ src/main/java/rota/dao/PersonRepository.java
@@ -3,6 +3,7 @@ package rota.dao;
 import java.util.List;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import rota.entities.Person;
@@ -15,5 +16,5 @@ import rota.entities.Person;
 @RepositoryRestResource(collectionResourceRel = "people", path = "people")
 public interface PersonRepository extends PagingAndSortingRepository<Person, Long> {
 
-    List<Person> findByLastName(String lastName);
+    List<Person> findByLastName(@Param(value = "name") String lastName);
 }
